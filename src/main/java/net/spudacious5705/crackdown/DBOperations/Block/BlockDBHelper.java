073d4159279ac81fd951491bb98c216cd433bdf8
Spudacious <spud.dev5705@gmail.com>
2025-11-22 13:14:17 +0000

    Created BlockDBHelper for easier serialisation of Blockstates

---
+++ src/main/java/net/spudacious5705/crackdown/DBOperations/Block/BlockDBHelper.java
@@ -0,0 +1,46 @@
+package net.spudacious5705.crackdown.DBOperations.Block;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public interface BlockDBHelper {
+    Pattern pattern = Pattern.compile("\\(([^)]+)\\)(?:\\[([^]]+)])?");
+    static AffectedBlock CreateAffectedBlock(BlockPos pos, BlockState state) {
+
+        BlockStateString s = getBlockStateAsString(state);
+
+        return new AffectedBlock(
+                pos.getX(),
+                pos.getY(),
+                pos.getZ(),
+                s.block,
+                s.state
+        );
+
+    }
+
+    static BlockStateString getBlockStateAsString(BlockState state) {
+
+        Matcher matcher = pattern.matcher(state.toString());
+
+        if (matcher.matches()) {
+            String blockString = matcher.group(1); // inside ()
+            String stateString = matcher.group(2); // inside []
+
+            return new BlockStateString(
+                    blockString,
+                    stateString
+            );
+        }
+
+        throw new IllegalStateException("[CRACKDOWN] BlockState.toString() is malformed or has unexpected structure: " + state);
+
+    }
+
+    record AffectedBlock(int x, int y, int z, String block, String state){}
+
+    record BlockStateString(String block, String state){}
+}
