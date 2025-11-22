041fbd04c12cf65b6a6e0e5e84af566e124cc4cd
Spudacious <spud.dev5705@gmail.com>
2025-11-22 13:49:04 +0000

    Logging for container close

--- src/main/java/net/spudacious5705/crackdown/events/listeners/CrackdownContainerListener.java
+++ src/main/java/net/spudacious5705/crackdown/events/listeners/CrackdownContainerListener.java
@@ -3,16 +3,10 @@ package net.spudacious5705.crackdown.events.listeners;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.inventory.AbstractContainerMenu;
 import net.minecraft.world.inventory.ContainerListener;
-import net.minecraft.world.inventory.Slot;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.level.block.entity.BlockEntity;
-import net.spudacious5705.crackdown.DBOperations.BlockEntity.BlockEntityInteraction;
-import net.spudacious5705.crackdown.DBOperations.Entity.EntityInteraction;
 import net.spudacious5705.crackdown.helper.GetDatabaseIdFunc;
 import net.spudacious5705.crackdown.logging.ItemStackChangeType;
 import org.jetbrains.annotations.NotNull;
-
-import java.lang.ref.WeakReference;
 
 public abstract class CrackdownContainerListener implements ContainerListener {
     final int playerDBID;
--- src/main/java/net/spudacious5705/crackdown/events/listeners/EntityContainerListener.java
+++ src/main/java/net/spudacious5705/crackdown/events/listeners/EntityContainerListener.java
@@ -4,7 +4,7 @@ import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.item.ItemStack;
-import net.spudacious5705.crackdown.DBOperations.Entity.EntityInteraction;
+import net.spudacious5705.crackdown.db_operations.Entity.EntityInteraction;
 import net.spudacious5705.crackdown.events.EventsUtil;
 import net.spudacious5705.crackdown.helper.GetDatabaseIdFunc;
 import net.spudacious5705.crackdown.logging.ItemStackChangeType;
