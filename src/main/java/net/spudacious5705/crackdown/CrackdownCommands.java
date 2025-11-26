1f06a87dc331742a81b2dc4f060a0a9791770c3b
Spudacious <spud.dev5705@gmail.com>
2025-11-26 15:43:47 +0000

    Commands class rename

--- src/main/java/net/spudacious5705/crackdown/CrackdownCommands.java
+++ src/main/java/net/spudacious5705/crackdown/CrackdownCommands.java
@@ -1,5 +1,6 @@
 package net.spudacious5705.crackdown;
 
+import net.minecraft.commands.Commands;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraftforge.event.RegisterCommandsEvent;
@@ -10,11 +11,11 @@ import net.spudacious5705.crackdown.helper.PlayerInfoFuc;
 import static net.spudacious5705.crackdown.events.PlayerEvents.acceptedRulesKey;
 
 @Mod.EventBusSubscriber(modid = Crackdown.MODID)
-public class Commands {
+public class CrackdownCommands {
     @SubscribeEvent
     public static void onRegisterCommands(RegisterCommandsEvent event) {
         event.getDispatcher().register(
-                net.minecraft.commands.Commands.literal("accept_rules")
+                Commands.literal("accept_rules")
                         .executes((ctx) -> {
                             if(ctx.getSource().getEntity() instanceof ServerPlayer player){
                                 var p =((PlayerInfoFuc)player);
