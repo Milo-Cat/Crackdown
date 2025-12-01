39b8ca22963476e81c0f13d93ba135929b5cd81a
Spudacious <spud.dev5705@gmail.com>
2025-12-01 17:19:04 +0000

    Base block entity search class

--- src/main/java/net/spudacious5705/crackdown/db_operations/block_entity/BlockEntitySearch.java
+++ src/main/java/net/spudacious5705/crackdown/db_operations/block_entity/BlockEntitySearch.java
@@ -5,19 +5,50 @@ import net.spudacious5705.crackdown.db_operations.PositionalResult;
 import net.spudacious5705.crackdown.db_operations.SQLOperation;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.function.Consumer;
 
 public class BlockEntitySearch extends SQLOperation {
 
+    final String selection_statement;
+
+    final Consumer<PreparedStatement> valueInputter;
+
     final Consumer<BlockEntitySearchResult> complete_action;
 
-    public BlockEntitySearch(Consumer<BlockEntitySearchResult> completeAction) {
-        complete_action = completeAction;
+    public BlockEntitySearch(String selectionStatement, Consumer<PreparedStatement> valueInputter, Consumer<BlockEntitySearchResult> completeAction) {
+        this.selection_statement = selectionStatement;
+        this.valueInputter = valueInputter;
+        this.complete_action = completeAction;
     }
 
     @Override
     public void accept(Connection connection) {
+        try {
+            PreparedStatement stmt = connection.prepareStatement("""
+                    SELECT d.name, b.x, b.y, b.z, i.timestamp, b.id, s.type, p.name, a.action, i.info
+                    FROM block_entity_interaction AS i
+                    LEFT JOIN block_entity AS b ON i.block_entity = b.id
+                    LEFT JOIN dimension AS d ON b.dimension = d.id
+                    LEFT JOIN source AS s ON i.source = s.id
+                    LEFT JOIN players AS p ON i.player = p.id
+                    LEFT JOIN block_entity_action_types AS a ON i.action = a.id
+                    
+                    """ + selection_statement);
 
+            //stmt.set ....
+            ResultSet rs = stmt.executeQuery();
+
+            //while(rs.next()){
+                //rs.getString();
+            //}
+
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     public static class BlockEntitySearchResult extends PositionalResult {
@@ -25,7 +56,7 @@ public class BlockEntitySearch extends SQLOperation {
         final int[] id;
         final String[] source;
         final String[] player_name;
-        final String[] actions;
+        final String[] action;
         final String[] info;
 
         final Consumer<BlockEntitySearchResult> complete_action;
@@ -38,7 +69,7 @@ public class BlockEntitySearch extends SQLOperation {
                 int[] id,
                 String[] source,
                 String[] playerName,
-                String[] actions,
+                String[] action,
                 String[] info,
                 Consumer<BlockEntitySearchResult> completeAction
         ) {
@@ -46,7 +77,7 @@ public class BlockEntitySearch extends SQLOperation {
             this.id = id;
             this.source = source;
             player_name = playerName;
-            this.actions = actions;
+            this.action = action;
             this.info = info;
             complete_action = completeAction;
         }
