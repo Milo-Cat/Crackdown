608ddd1b5f03b9c6e88996d55a9e73e2d39763cb
Spudacious <spud.dev5705@gmail.com>
2026-01-03 18:42:31 +0000

    Moved BlockEntitySearchResult to it's own class in lookup package

---
+++ src/main/java/net/spudacious5705/crackdown/lookup/BlockEntitySearchResult.java
@@ -0,0 +1,42 @@
+package net.spudacious5705.crackdown.lookup;
+
+import net.minecraft.core.BlockPos;
+
+import java.util.function.Consumer;
+
+public class BlockEntitySearchResult extends PositionalResult {
+
+        final int[] id;
+        final String[] source;
+        final String[] player_name;
+        final String[] action;
+        final String[] info;
+
+        final Consumer<BlockEntitySearchResult> complete_action;
+
+        protected BlockEntitySearchResult(
+                String[] dimension,
+                BlockPos[] blockPos,
+                int resultCount,
+                long[] timestamp,
+                int[] id,
+                String[] source,
+                String[] playerName,
+                String[] action,
+                String[] info,
+                Consumer<BlockEntitySearchResult> completeAction
+        ) {
+            super(dimension, blockPos, resultCount, timestamp);
+            this.id = id;
+            this.source = source;
+            player_name = playerName;
+            this.action = action;
+            this.info = info;
+            complete_action = completeAction;
+        }
+
+        @Override
+        public void complete() {
+            complete_action.accept(this);
+        }
+    }
